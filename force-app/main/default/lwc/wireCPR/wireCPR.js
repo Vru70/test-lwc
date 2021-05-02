2f47803d1b966bd590c0ca41d6db0fe1028101c0
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-02 20:56:05 +0530

    wire test

--- force-app/main/default/lwc/wireCPR/wireCPR.js
+++ force-app/main/default/lwc/wireCPR/wireCPR.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+/**
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 05-02-2021
+ * @last modified by  : Vrushabh Uprikar
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   05-02-2021   Vrushabh Uprikar   Initial Version
+**/
+import { LightningElement, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 
-export default class WireCPR extends LightningElement {}
+export default class WireCPR extends LightningElement {
+    @wire(CurrentPageReference) pageRef;
+
+    get currentPageRef() {
+        return this.pageRef ? JSON.stringify(this.pageRef, null, 2) : '';
+    }
+}
