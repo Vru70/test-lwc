fb5253b26285c6feb6c81296707dafab8baaa9a3
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-02 14:29:13 +0530

    first commit

---
+++ force-app/main/default/lwc/unitTest/unitTest.js
@@ -0,0 +1,14 @@
+/**
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 05-02-2021
+ * @last modified by  : Vrushabh Uprikar
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   05-02-2021   Vrushabh Uprikar   Initial Version
+**/
+import { LightningElement, api } from 'lwc';
+import { sum } from './sum';
+
+export default class UnitTest extends LightningElement {
+    @api unitNumber = sum(2, 3);
+}
